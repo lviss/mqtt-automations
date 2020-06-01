0ad5a2b045747ddbc1f150db62a613f605a2ad84
lviss <lviss@users.noreply.github.com>
2020-06-01 16:17:22 -0700

    Added an action for turning on the lights when I get out of bed
    Updated the action to dim the lights when a movie starts because
    I got more smart lights in there :)

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ var mqtt = require('mqtt');
 var mqttclient  = mqtt.connect(config.mqtt_server);
 var plex_tv_id = 'mhi9x8y9k4fmwblbcbyoeobv';
 
+// keep track of states of things
+let last_state = false;
+let last_reading_occupied = false;
+
 mqttclient.on('connect', function () {
   mqttclient.subscribe('octoPrint/event/PrintDone');
   mqttclient.subscribe('plex/' + plex_tv_id);
+  mqttclient.subscribe('devices/master_bedroom/bed/load');
 })
 
 mqttclient.on('message', function (topic, messageBuf) {
@@ -24,9 +29,47 @@ mqttclient.on('message', function (topic, messageBuf) {
      Dim lights when plex starts playing
   *****************************************/
   if (topic == 'plex/' + plex_tv_id) {
-    if (message == 'media.play' || message == 'media.resume')
-      mqttclient.publish('devices/kitchen/lights/level/set', "0");
-    if (message == 'media.pause' || message == 'media.stop')
-      mqttclient.publish('devices/kitchen/lights/level/set', "99");
+    if (message == 'media.play' || message == 'media.resume') 
+      setLivingRoomLights("0");
+    if (message == 'media.pause' || message == 'media.stop') 
+      setLivingRoomLights("99");
+  }
+
+  /*****************************************
+     Turn off lights when I get in bed
+  *****************************************/
+  if (topic == 'devices/master_bedroom/bed/load') {
+    // normal readings appear to be 15 when unoccupied and 35 when occupied.
+    let weight = parseInt(message);
+    // filter out any crazy numbers
+    if (weight > 65 || weight < 10) return;
+    let occupied = weight > 25;
+    // require two readings in a row to change state
+    let changed = (occupied != last_state) && (occupied == last_reading_occupied);
+    if (changed) {
+      mqttclient.publish('devices/master_bedroom/light' + '/dimming_duration/set', "5");
+      mqttclient.publish('devices/master_bedroom/light' + '/level/set', occupied ? '0' : '50');
+      let alert_text = 'Turning ' + (occupied ? 'off' : 'on') + ' light because bed reads ' + weight;
+      console.log(alert_text);
+      mqttclient.publish('notify/me', '{"message":"' + alert_text + '"}');
+      last_state = occupied;
+    }
+    last_reading_occupied = occupied;
   }
 });
+
+function setLivingRoomLights(level) {
+  let duration = "5";
+  let devices = [
+    'devices/kitchen/lights3',
+    'devices/kitchen/lights4',
+    'devices/family_room/fanlight'
+  ];
+  // set a duration for each light
+  for (let i = 0; i < devices.length; i++)
+    mqttclient.publish(devices[i] + '/dimming_duration/set', duration);
+  // set the level for each light
+  for (let i = 0; i < devices.length; i++)
+    mqttclient.publish(devices[i] + '/level/set', level);
+}
+
